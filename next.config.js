cb67c08f9f63ce4874514641afe5bfd31f780f3f
IsdbSscDev <sscdeveloper@isdb.org>
2022-09-09 14:17:55 +0530

    API Initialization

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    BASE_URL: "http://localhost:3000/",
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import { HomeContainer } from "../App/container";
 
-export default function Home() {
+export default function Home({ cate }) {
+  console.log(cate);
   return (
     <>
       <HomeContainer />
     </>
   );
+}
+
+export async function getStaticProps(context) {
+  const res = await fetch(process.env.BASE_URL + `/api`);
+  const cate = await res.json();
+  return {
+    props: { cate }, // will be passed to the page component as props
+  };
 }
