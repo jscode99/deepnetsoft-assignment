96127f7b5e89b445bcf442e73658437b32750375
IsdbSscDev <sscdeveloper@isdb.org>
2022-09-09 14:21:00 +0530

    API Initialization

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    BASE_URL: "http://localhost:3000/",
+    BASE_URL: "http://localhost:3000",
   },
   reactStrictMode: true,
   swcMinify: true,
